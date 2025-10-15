d72a448773f443e895e46f2c922a7a21a951ff1f
puzzoviodev <silviopuzz@gmail.com>
2025-10-15 20:42:05 -0300

    01

---
+++ medicSearch/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Profile)
+admin.site.register(State)
+admin.site.register(City)
+admin.site.register(Neighborhood)
+admin.site.register(Address)
+admin.site.register(DayWeek)
+admin.site.register(Rating)
+admin.site.register(Speciality)
+from django.contrib import admin
+
+# Register your models here.
