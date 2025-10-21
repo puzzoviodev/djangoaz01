d7cb829ed0da7c4221daeedf25494bc17e651bb6
puzzoviodev <silviopuzz@gmail.com>
2025-10-21 09:14:13 -0300

    01

--- medicSearch/admin.py
+++ medicSearch/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import *
+class ProfileAdmin(admin.ModelAdmin):
+# Cria um filtro de hierarquia com datas
+    date_hierarchy = 'created_at'
 
-admin.site.register(Profile)
+
+admin.site.register(Profile,ProfileAdmin)
 admin.site.register(State)
 admin.site.register(City)
 admin.site.register(Neighborhood)
